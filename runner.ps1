c293e86d7b8a5a99d3bfe7bd6e7eb06968fa1d84
Gavin Eke <GavinEke@users.noreply.github.com>
2023-11-03 16:20:33 +1100

    Initial Commit of PowerShell Script

---
+++ runner.ps1
@@ -0,0 +1,32 @@
+# Get data and diff
+$oldJson = Get-Content -Raw -Path data.json | ConvertFrom-Json
+$newJson = Invoke-WebRequest -Uri https://raw.githubusercontent.com/joshhighet/ransomwatch/main/posts.json | ConvertFrom-Json
+$jsonDiff = Compare-Object $oldJson $newJson
+
+# Loop through each new entry
+ForEach ($Item in $jsonDiff) {
+    # Structure Teams Message
+    $JSONBody = [PSCustomObject][Ordered]@{
+        "@type" = "MessageCard"
+        "@context" = "<http://schema.org/extensions>"
+        "summary" = "RansomWatch Alert!: $($Item.InputObject.post_title)"
+        "themeColor" = '0078D7'
+        "title" = "RansomWatch Alert!"
+        "text" = "`nCompany: $($Item.InputObject.post_title)" +
+            "`nRansom Group: $($Item.InputObject.group_name)" +
+            "`nDiscovered: $($Item.InputObject.discovered)"
+    }
+    $TeamsMessageBody = ConvertTo-Json $JSONBody
+
+    # Post Teams Message to Webhook
+    $IRMParams = @{
+        "URI" = $env:TEAMS_WEBHOOK_URL
+        "Method" = 'POST'
+        "Body" = $TeamsMessageBody
+        "ContentType" = 'application/json'
+    }
+    Invoke-RestMethod @parameters
+}
+
+# Override data.json with new data
+$newJson | Out-File data.json -Force
